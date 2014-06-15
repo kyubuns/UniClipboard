b3b32148ddbff308cac099639c7aae77dd868e43
kyubuns <jp@ex.cx>
2014-06-15 22:35:57 +0900

    native plugin繋ぎ込み

--- Assets/UniClipboard/UniClipboard.cs
+++ Assets/UniClipboard/UniClipboard.cs
@@ -5,14 +5,22 @@ public static class UniClipboard
 {
 	public static string value
 	{
-		get
-		{
-			return UniClipboardHelper.PC.clipBoard;
-		}
-
 		set
 		{
+#if UNITY_IOS && !UNITY_EDITOR
+			UniClipboardHelper.iOS.SetClipboard(value);
+#else
 			UniClipboardHelper.PC.clipBoard = value;
+#endif
+		}
+
+		get
+		{
+#if UNITY_IOS && !UNITY_EDITOR
+			return UniClipboardHelper.iOS.GetClipboard();
+#else
+			return UniClipboardHelper.PC.clipBoard;
+#endif
 		}
 	}
 }
---
+++ Assets/UniClipboard/iOSClipboardHelper.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace UniClipboardHelper
+{
+
+#if UNITY_IOS
+public static class iOS
+{
+    [DllImport("__Internal")]
+    private static extern void _SetClipboard(string value);
+
+    [DllImport("__Internal")]
+    private static extern string _GetClipboard();
+
+	public static void SetClipboard(string value)
+	{
+		_SetClipboard(value);
+	}
+
+	public static string GetClipboard()
+	{
+		return _GetClipboard();
+	}
+}
+#endif
+
+}
