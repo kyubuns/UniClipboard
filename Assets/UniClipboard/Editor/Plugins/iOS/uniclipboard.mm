c7ed62c364261837cb82d43528accd32f5b9cbee
kyubuns <jp@ex.cx>
2014-07-16 00:11:35 +0900

    別のアセットと一緒に使ってるときに名前衝突したのでどうにかした

--- Assets/UniClipboard/Editor/Plugins/iOS/uniclipboard.mm
+++ Assets/UniClipboard/Editor/Plugins/iOS/uniclipboard.mm
@@ -2,12 +2,12 @@
 #error This file must be compiled with ARC. Either turn on ARC for the project or use -fobjc-arc flag
 #endif
 
-NSString* CreateNSString(const char* string)
+NSString* UniClipboard_CreateNSString(const char* string)
 {
 	return [NSString stringWithUTF8String: string ? string : ""];
 }
 
-char* MakeStringCopy(const char* string)
+char* UniClipboard_MakeStringCopy(const char* string)
 {
 	if (string == NULL) return NULL;
 	char* res = (char*)malloc(strlen(string) + 1);
@@ -19,7 +19,7 @@ extern "C"
 {
 	void _SetClipboard(const char* text)
 	{
-		NSString *nsstringText = CreateNSString(text);
+		NSString *nsstringText = UniClipboard_CreateNSString(text);
 		UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
 		[pasteboard setValue:nsstringText forPasteboardType:@"public.text"];
 	}
@@ -28,6 +28,6 @@ extern "C"
 	{
 		UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
 		NSString *string = [pasteboard valueForPasteboardType:@"public.text"];
-		return MakeStringCopy([string UTF8String]);
+		return UniClipboard_MakeStringCopy([string UTF8String]);
 	}
 }
