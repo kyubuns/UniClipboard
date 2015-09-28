d5563e9a8c8c4051c486c24412dea9c4b586a92d
kyubuns <kyubuns@gmail.com>
2015-09-28 14:43:41 +0900

    Unity5.2対応

--- Assets/UniClipboard/PCClipboardHelper.cs
+++ Assets/UniClipboard/PCClipboardHelper.cs
@@ -10,30 +10,15 @@ namespace UniClipboardHelper
 
 public class PC
 {
-	private static PropertyInfo m_systemCopyBufferProperty = null;
-	private static PropertyInfo GetSystemCopyBufferProperty()
-	{
-		if (m_systemCopyBufferProperty == null)
-		{
-			Type T = typeof(GUIUtility);
-			m_systemCopyBufferProperty = T.GetProperty("systemCopyBuffer", BindingFlags.Static | BindingFlags.NonPublic);
-			if (m_systemCopyBufferProperty == null)
-				throw new Exception("Can't access internal member 'GUIUtility.systemCopyBuffer' it may have been removed / renamed");
-		}
-		return m_systemCopyBufferProperty;
-	}
-
 	public static string clipBoard
 	{
 		get
 		{
-			PropertyInfo P = GetSystemCopyBufferProperty();
-			return (string)P.GetValue(null,null);
+			return GUIUtility.systemCopyBuffer;
 		}
 		set
 		{
-			PropertyInfo P = GetSystemCopyBufferProperty();
-			P.SetValue(null,value,null);
+			GUIUtility.systemCopyBuffer = value;
 		}
 	}
 }
